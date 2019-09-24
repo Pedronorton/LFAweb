6642be4628355c1f7bb06d0220e6de89dc906de9
Marcos-Pimentel <marcos.salles@estudante.ufla.br>
2019-09-24 11:57:03 -0300

    :hammer: Menu 'implementado'

--- frontEnd/interface/src/interface/Header.jsx
+++ frontEnd/interface/src/interface/Header.jsx
@@ -5,6 +5,7 @@ import './css/Header.css';
 function App() {
   return (
     <React.Fragment>
+			<head></head>
         <header className="navbar navbar-dark bg-dark shadow-sm">
             <div className="navbar-brand">
 								<strong><i className="fa fa-home"></i>LfaAppWeb - </strong>
--- frontEnd/interface/src/interface/Main.jsx
+++ frontEnd/interface/src/interface/Main.jsx
@@ -1,46 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './css/Main.css';
 import Lambda from './img/icons8-lambda-24.png';
 
-function App() {
-  return (
-    <React.Fragment>
-      
-      <div className="row">
-        <div className="col-12 col-sm-7" id="text-area">
-          <div className="palavra">
-            <>Word: <input className="campo"/></> <br/>
-						<i className="descricao">If you want to verify acceptances and derivations</i>
-          </div>
-          <div className="gramatica">
-            <p>Grammar: <textarea className="campo" /></p>
-          </div>
-		  		<div>
-						<br/>
-					  <button type="button" className="btn btn-primary btn-m" id="ok">OK</button>
-		  		</div>
-        </div>
-        <div className="col-12 col-lg-3 col-md-4 col-sm-5">
-					<section className="container grid grid-template-columns-1">
-						<div className = "item subgrid">
-							<button type = "button" className = "btn btn-primary btn-s"><b id="lambda">λ</b>{/*<img src={Lambda} alt="Lambda"></img>*/}</button>
-							<button type = "button" className = "btn btn-primary btn-s" id="">|</button>
-							<button type = "button" className = "btn btn-primary btn-s" id="">-></button>
+
+
+class Main extends Component {
+	
+	
+	lambdaFunction() {
+		document.getElementById("grammar-text").value=document.getElementById("grammar-text").value+" λ ";
+		
+	}
+	pipeFunction() {
+		document.getElementById("grammar-text").value=document.getElementById("grammar-text").value+" | ";
+	}
+	
+	arrowFunction() {
+		document.getElementById("grammar-text").value=document.getElementById("grammar-text").value+" -> ";
+	}
+	
+	limparFunction() {
+		document.getElementById("grammar-text").value="";
+	}
+	
+	historicoFunction() {
+		alert("Sem Histórico");
+	}
+	
+	informacoesFunction() {
+		alert("Em desenvolvimento, aguarde um tempo...")
+	}
+	
+	render() {
+		return (
+			<React.Fragment>
+				
+				<div className="row">
+					<div className="col-12 col-sm-7" id="text-area">
+						<div className="palavra">
+							<>Word: <input className="campo"/></> <br/>
+							<i className="descricao">If you want to verify acceptances and derivations</i>
 						</div>
-						<div className = "item">
-							<button type = "button" className = "btn btn-primary btn-m">Limpar</button>
+						<div className="gramatica">
+							<p>
+								Grammar: <textarea className="campo" id="grammar-text" />
+							</p>
 						</div>
-						<div className = "item">
-							<button type = "button" className = "btn btn-primary btn-m">Histórico</button>
+						<div>
+							<br/>
+							<button type="button" className="btn btn-primary btn-m" id="ok">OK</button>
 						</div>
-						<div className = "item">
-							<button type = "button" className = "btn btn-primary btn-m"><span className="fa fa-exclamation-circle"></span></button>
-						</div>
-					</section>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+					</div>
+					<div className="col-12 col-lg-3 col-md-4 col-sm-5">
+						<section className="container grid grid-template-columns-1">
+							<div className = "item subgrid">
+								<button type = "button" className = "btn btn-primary btn-s" onClick={this.lambdaFunction}><b id="lambda">λ</b></button>
+								<button type = "button" className = "btn btn-primary btn-s" onClick={this.pipeFunction}>|</button>
+								<button type = "button" className = "btn btn-primary btn-s" onClick={this.arrowFunction}>-></button>
+							</div>
+							<div className = "item">
+								<button type = "button" className = "btn btn-primary btn-m" onClick={this.limparFunction}>Limpar</button>
+							</div>
+							<div className = "item">
+								<button type = "button" className = "btn btn-primary btn-m" onClick={this.historicoFunction}>Histórico</button>
+							</div>
+							<div className = "item">
+								<button type = "button" className = "btn btn-primary btn-m" onClick={this.informacoesFunction}><span className="fa fa-exclamation-circle"></span></button>
+							</div>
+						</section>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
-export default App;
+export default Main;
